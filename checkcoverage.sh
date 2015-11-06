8471193c1e55a40cd205adaffe3a171edcdfd3cd
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2015-11-06 13:37:45 +0100

    updates to test

--- checkcoverage.sh
+++ checkcoverage.sh
@@ -16,6 +16,8 @@ if [ $# -gt 2 ]; then
     STRICT=1
 fi
 
+STOPEXIT=0
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 
     IFS='|' read -a mylinearray <<< "$line"
@@ -32,14 +34,15 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     if [[ $file_nesp =~ ^.*\.(js)$ ]]  && [[ ! $file_nesp =~ ^.*\.(spec\.js)$ ]]; then
 
         if [[ $lines -lt $LIMIT || ( $STRICT -ge $STRICTVALUE  && ( $branch -lt $LIMIT || $func -lt $LIMIT || $stmts -lt $LIMIT ) ) ]]; then
+
+            printf "\n${file_nesp}:"
             echo "-------------"
-            echo "${file_nesp}:"
-            echo "-------------"
+            STOPEXIT=1
         fi
 
         if [[ $lines -lt $LIMIT ]]; then
             printf "%-30s %s\n" "Line coverage:" "${lines}%"
-            printf "%-30s %s\n" "Line numbers:" ${unc}
+            printf "%-30s %s\n" "Line numbers not covered:" ${unc}
         fi
 
         if [[ $STRICT -ge $STRICTVALUE ]]; then
@@ -61,3 +64,6 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     fi
 
 done < "$FILE"
+
+printf "\n\nExiting with code: $STOPEXIT\n\n"
+exit $STOPEXIT
