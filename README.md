98a7cfee8676b3e3f944f88cb25cfedafc649fbc
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2015-11-06 13:22:03 +0100

    updates to test

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # karma-coverage-git-pre-commit-hook 
 
 This is a pre-commit hook designed for Karma test coverages.
-It checks all files' statements, branches, functions & line to check if they have a certain amount of coverage (%).
+It checks all files' statements, branches, functions and lines to check if they have a certain amount of coverage (%).
 
 ## How to use git hooks
 
-You can find a some nice explanations about git hooks on this page:
-
-[http://githooks.com/](http://githooks.com/)
+You can find a some nice explanations about git hooks on this page: [http://githooks.com/](http://githooks.com/)
--- checkcoverage.sh
+++ checkcoverage.sh
@@ -1,4 +1,67 @@
 #!/bin/bash
+
+FILE=$1
+if [ $# -lt 1 ]; then
+    FILE="test/coverage/coverage.txt"
+fi
+
+LIMIT=$2
+if [ $# -lt 2 ]; then
+    LIMIT=80
+fi
+
+STRICT=0
+STRICTVALUE=1
+if [ $# -gt 2 ]; then
+    STRICT=1
+fi
+
+echo $FILE
+echo $LIMIT
+echo $STRICT
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "Text read from file: $line"
-done < "$1"
+
+    IFS='|' read -a mylinearray <<< "$line"
+
+    file="$(echo -e "${mylinearray[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    stmts=${mylinearray[1]%.*}
+    branch=${mylinearray[2]%.*}
+    func=${mylinearray[3]%.*}
+    lines=${mylinearray[4]%.*}
+    unc=${mylinearray[5]}
+
+    file_nesp="$(echo -e "${file}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+
+    if [[ $file_nesp =~ ^.*\.(js)$ ]]  && [[ ! $file_nesp =~ ^.*\.(spec\.js)$ ]]; then
+
+        if [[ $lines -lt $LIMIT || ( $STRICT -ge $STRICTVALUE  && ( $branch -lt $LIMIT || $func -lt $LIMIT || $stmts -lt $LIMIT ) ) ]]; then
+            echo "-------------"
+            echo "${file_nesp}:"
+            echo "-------------"
+        fi
+
+        if [[ $lines -lt $LIMIT ]]; then
+            echo "Line coverage:       ${lines}%"
+            echo "Lines not covered:   ${unc}"
+        fi
+
+        if [[ $STRICT -ge $STRICTVALUE ]]; then
+
+            if [[ $stmts -lt $LIMIT ]]; then
+                echo "Statement coverage:  ${stmts}%"
+            fi
+
+            if [[ $branch -lt $LIMIT ]]; then
+                echo "Branch coverage:     ${branch}%"
+            fi
+
+            if [[ $func -lt $LIMIT ]]; then
+                echo "Function coverage:   ${func}%"
+            fi
+
+        fi
+
+    fi
+
+done < "$FILE"
